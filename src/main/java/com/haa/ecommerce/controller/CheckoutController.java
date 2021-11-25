ae4d24e4bd3f480c05a917a484a7f784cb3a9fbb
HamzaAlaya91 <alaya.hamza1@gmail.com>
2021-11-25 21:53:41 +0100

    secure HTTPS openssl

--- src/main/java/com/haa/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/haa/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,7 @@ import com.haa.ecommerce.dto.PurchaseResponse;
 import com.haa.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
